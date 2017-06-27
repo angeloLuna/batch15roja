51c28050afd9c8078c78098ec1c409f607103733
angel.luna.najera@gmail.com <angel.luna.najera@gmail.com>
2017-06-26 21:51:05 -0500

    ejercicio de convertir la primera palabra de un string a mayusculas

---
+++ estructuras_basicas_python/strings.py
@@ -0,0 +1,8 @@
+frase='agua pasa por mi casa'
+frase=frase.split(' ')
+frase_lista=[]
+for palabra in frase:
+   buf=palabra[0].upper()+palabra[1:]
+   frase_lista.append(buf)
+frase=' '.join(frase_lista)
+print frase
